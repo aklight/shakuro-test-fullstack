ec221bf24be3ee2432555ecb4263347df14a1e18
Aleksandr Kudriavtsev <aklightcode@gmail.com>
2018-10-20 16:05:36 +0300

    Applied connection to the database by using mongoose

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+const dbLogin = "admin";
+const dbPassword = "admin123456";
+
+module.exports = {
+  mongoURI: `mongodb://${dbLogin}:${dbPassword}@ds237373.mlab.com:37373/shakuro-test`
+};
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+// DB Config
+
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log(err));
+
+// Body parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.json("Hello world!"));
 
